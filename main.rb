884b800a78f47b34a6b536679a189b11c0001d18
Suchitra Edussuriya <sse_p@hotmail.com>
2013-10-16 10:47:04 -0700

    adding player_name form and included code to handle empty name string

--- main.rb
+++ main.rb
@@ -1,29 +1,36 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
 get '/' do 
-	code = "My name is Suchitra"
-  erb code
-  
+  if session[:p_name]
+    redirect '/bet'
+  else
+    redirect '/set_name'
+  end
 end
 
-
-
-get '/profile' do
-  erb :profile
+get '/set_name' do
+  if session[:check]
+    @error = "Name can't be empty. Please enter a valid name"
+    erb :player_name
+  else
+    erb :player_name
+  end
 end
 
-get '/subtest/login' do
-  erb :'subtest/login'
+post '/set_name' do
+  if params[:p_name] == ""
+    session[:check] = 1
+    redirect '/set_name'
+  else
+    session[:p_name] = params[:p_name]
+  end
+  redirect '/bet'
 end
 
-get '/foo' do
-  redirect to('/profile'), 303
-
+get '/bet' do
+  erb :bet
 end
-
-
-
-
